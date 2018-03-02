74eaa32fbc17521945f6195ea536c9c5a4c5b7e2
ozgun <ozgunozdemir92@gmail.com>
2018-03-02 20:31:42 +0300

    added create.jade

--- address-book/routes/users.js
+++ address-book/routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
 
 router.get('/', function(req, res, next) {
   res.render('list');
